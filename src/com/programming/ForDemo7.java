56db649579d9da17f7e53f17d472eb46fe7c07dc
vikashQA92 <vikashQA92@gmail.com>
2023-08-03 18:00:14 +0530

    Added Star programme

--- src/com/programming/ForDemo7.java
+++ src/com/programming/ForDemo7.java
@@ -4,9 +4,11 @@ public class ForDemo7 {
 	
 	public static void main(String[] args) {
 		
+		System.out.println("My Name is Vikash Gupta");
 		for(int i= 1; ; System.out.println("Hi"))
 		{
 			System.out.println("i :" +i);
+			
 		}
 	}
 
